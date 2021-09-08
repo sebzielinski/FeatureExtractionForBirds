c5f809841f4417a503c76f3dcfd3be6b5b5e560f
sebzielinski <szielinski@uni-osnabrueck.de>
2021-09-08 22:00:12 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FeatureExtractionForBirds
 Feature Extraction for Recognizing individual birds based on their calls
+
+For extracting the features, pyAudioAnalysis library has been used:
+https://github.com/tyiannak/pyAudioAnalysis
+
+For dynamic time warping, dtw-python has been used:
+https://dynamictimewarping.github.io/python/
+
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import ShortTermFeatures
 from dtw import *
+import scipy as scipy
 import numpy as np
-
 import matplotlib.pyplot as plt
-[Fs, x] = audioBasicIO.read_audio_file("data/m12s1_1.wav")
+
+dyn_time_warp = False
+cross_corr = True
+
+# use pyAudioAnalysis to read audio files
+[Fs, x] = audioBasicIO.read_audio_file("data/mitnoise.wav")
 # ShortTermFeatures.spectrogram(x, Fs, 0.050*Fs, 0.025*Fs, 1, 1)
+
+# use pyAudioAnalysis to extract relevant features
 F, f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.050*Fs, 0.025*Fs)
 # plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[0])
 # plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[1]); plt.show()
@@ -17,7 +24,7 @@ print("Feature Vectors: ", F.shape)
 # for i in range(len(F)):
 #     print(F[i][8:21])
 
-[Fs, x] = audioBasicIO.read_audio_file("data/m12s1_2.wav")
+[Fs, x] = audioBasicIO.read_audio_file("data/m16s1_2.wav")
 # ShortTermFeatures.spectrogram(x, Fs, 0.050*Fs, 0.025*Fs, 1, 1)
 F_2, f_names_2 = ShortTermFeatures.feature_extraction(x, Fs, 0.050*Fs, 0.025*Fs)
 # plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[0])
@@ -30,17 +37,27 @@ print("Feature Vectors: ", F_2.shape)
 # for i in range(len(F)):
 #     print(F[i][8:21])
 
+# use MFCC features only (index 8:21)
 query = F_2[8:21, :]
-template = F[8:21, 0:247]
+template = F[8:21, :]
 
 print("Query dimensions: ", query.shape, type(query))
 print("template dimensions: ", template.shape, type(query))
 
 # dynamic time warping (requires feature vectors of same dimensions)
-alignment = dtw(query, template, keep_internals=True)
-alignment.plot(type="threeway")
+# limitation: can only be used on audio with exact same length
+if (dyn_time_warp):
+    alignment = dtw(query, template, keep_internals=True)
+    print("alignement: ", type(alignment))
+    print(alignment)
+    alignment.plot(type="threeway")
 
-# cross correlation
-cc = np.correlate(query.flatten(), template.flatten())
-print("cross correlated array: ", cc.shape)
-print(cc)
+if (cross_corr):
+    # cross correlation (can be used on audio with different length)
+    cc = scipy.signal.correlate2d(query, template)
+    print("cross correlated array: ", cc.shape, type(cc))
+    # find index of match
+    y, x = np.unravel_index(np.argmax(cc), cc.shape)
+    print(cc[y, x], y, x)
+    plt.plot(cc[y, :])
+    plt.show()
